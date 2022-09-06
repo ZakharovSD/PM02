304af5a3691f7672cbd0c766f80e2120260bd481
Sergei Zacharov <rr1960@inbox.ru>
2022-09-06 17:05:20 +0300

    2 commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,3 +2,23 @@
 {
     internal class Program
     {
+        const double ProcBank = 0.08;
+
+        private static double getProcent(double cash)
+        {
+            double money = 0;
+            double procent = 0;
+            while (money <= cash)
+            {
+                if (cash >= 700000 && cash <= 749999.99)
+                {
+                    procent = 0.2;
+                    break;
+                }
+
+                if (money == 700000)
+                {
+                    procent = 0.2;
+                    money += 50000;
+                    continue;
+                }
