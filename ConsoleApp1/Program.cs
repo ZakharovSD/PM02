9d40227d29067e8d5d14b077b8f53121fd3a8edd
Sergei Zacharov <rr1960@inbox.ru>
2022-09-06 17:27:33 +0300

    diagram

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,40 +8,40 @@
         {
             double dengi = 0;
             double stavka = 0;
-            
-            
-            
+
+
+
             while (dengi <= cash)
             {
-                
-                
-                
+
+
+
                 if (dengi == 700000)
                 {
                     stavka = 0.2;
                     dengi += 50000;
                     continue;
                 }
-                
-                
-                
+
+
+
                 if (cash >= 700000 && cash <= 749999.99)
                 {
                     stavka = 0.2;
                     break;
                 }
-               
-                
-                
+
+
+
                 if (dengi < 750000)
                 {
                     dengi += 49999.99;
                     stavka += 0.01;
                     dengi += 0.01;
                 }
-                
-                
-                
+
+
+
                 else
                 {
                     dengi += 49999.99;
@@ -49,13 +49,50 @@
                     dengi += 0.01;
                 }
             }
-            
-            
-            
+
+
+
             return Math.Round(stavka, 2);
-       
-        
-        
+
+
+
         }
+    }
+    private static double getMoney(double cash, double stavka, int years)
+    {
+
+
+
+        for (int i = 1; i <= years * 12; i++)
+        {
+
+
+
+            if (i % 3 == 0)
+            {
+                stavka += 0.005;
+            }
+
+
+
+            if (stavka - StavkaBank > 5)
+            {
+                cash += cash * stavka * 0.7;
+            }
+
+
+
+            else
+            {
+                cash += cash * stavka;
+            }
+        }
+
+
+
+        return Math.Round(cash, 2);
+
+
+
     }
 }
